2ab4225dc4c29d71b878df2e4636d6e860f3c11e
0xngmi <0xngmi@protonmail.com>
2022-12-01 11:20:23 +0000

    remove charts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import {setUnlistedProtocols, triggerUnlistedAlarms} from './unlistedAlerts'
 import { checkApiStatus } from './checkApiServer'
 import { getMostVisitedPages } from './reports/buildReport';
 import axios from 'axios'
-import { draw } from './reports/chart';
 
 const PORT = process.env.PORT || 5000;
 
@@ -39,55 +38,6 @@ app.use('/rebuild-server', (request: Request, response: Response) => {
   })
   response.sendStatus(200);
 });
-
-app.use('/yield-chart/:id', async (request: Request, response: Response) => {
-try{
-  const data = await axios.get(`https://yields.llama.fi/chart/${request.params.id}`)
-  const lastDay = new Date(Date.now() - 30*24*3600e3)
-  const dataset = data.data.data.filter(({timestamp}:any)=>new Date(timestamp) > lastDay)
-  const firstAPy = dataset[0].apy
-  const lastApy = dataset[dataset.length-1].apy
-  const image = await draw({
-    type: 'line' as any,
-    data: {
-      datasets: [{
-        data: dataset.map(({ apy, timestamp }: any) => ({ x: timestamp, y: apy })),
-        borderColor: lastApy > firstAPy? "#90ee90":"#ff6961",
-      }],
-    },
-    options: {
-      scales: {
-        x: {
-          display: false,
-        },
-        y: {
-          display: false,
-        },
-      },
-      plugins: {
-        legend: {
-          display: false
-        },
-      },
-      elements: {
-        point: {
-          radius: 0
-        }
-      }
-    },
-  }, {
-    height: 30,
-    width: 90,
-  })
-  response.set("Content-Disposition", "inline;");
-  response.set("Cache-Control", "max-age=3600");
-  response.contentType('image/png');
-  response.send(image);
-} catch(e){
-  response.sendStatus(400);
-}
-});
-
 
 app.use(express.urlencoded({ extended: true }));
 
