04554b2941bcd7832ff1c5a344038ac5d9845ab6
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-30 15:38:09 +0100

    update code to clear protocol data cache

--- src/commands/deleteCache.ts
+++ src/commands/deleteCache.ts
@@ -1,28 +1,32 @@
 import Command from "./commandInterface";
 import { Message } from "discord.js";
 import { getRawLiteProtocols } from './utils'
-import {CommandParser} from "../models/commandParser"
+import { CommandParser } from "../models/commandParser"
 import { deleteObjects } from "../utils/r2";
+import axios from "axios";
 
 export class DeleteCacheCommand implements Command {
   commandNames = ["delete-cache"];
 
-  async run(message: Message, parsed:CommandParser): Promise<string> {
+  async run(message: Message, parsed: CommandParser): Promise<string> {
     let protocolName = parsed.args.join(" ")
     const isTreasury = protocolName.endsWith("(treasury)")
-    if(isTreasury){
+    if (isTreasury) {
       protocolName = protocolName.slice(0, -" (treasury)".length)
     }
-    const {protocols, parentProtocols} = await getRawLiteProtocols()
-    let protocol = protocols.concat(parentProtocols).find((p:any)=>p.name.toLowerCase() === protocolName.toLowerCase())
-    if(protocol === undefined){
-        return "No protocol with that name!"
+    const { protocols, parentProtocols } = await getRawLiteProtocols()
+    let protocol = protocols.concat(parentProtocols).find((p: any) => p.name.toLowerCase() === protocolName.toLowerCase())
+    if (protocol === undefined) {
+      return "No protocol with that name!"
     }
     let protocolId = protocol.defillamaId ?? protocol.id
-    if(isTreasury){
+    if (isTreasury) {
       protocolId += "-treasury"
     }
-    await deleteObjects(protocolId)
+    // await deleteObjects(protocolId)
+    const { API2_SERVER_URL } = process.env
+    if (!API2_SERVER_URL) return 'Missing required env var: API2_SERVER_URL'
+    await axios.delete(`${API2_SERVER_URL}/debug-pg/tvl-cache-daily/${protocolId}`)
     return "Objects deleted"
   }
 }
